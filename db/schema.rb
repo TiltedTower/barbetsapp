0ff6d5892e342c8759c82b43b74dd98cf54d1183
Nick Peters <nick@barbetsapp.com>
2015-02-12 13:58:12 -0700

    add email and password columns to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150205212032) do
+ActiveRecord::Schema.define(version: 20150212205551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20150205212032) do
     t.string "name"
     t.string "hometown"
     t.date   "birthday"
+    t.string "email"
+    t.string "password_digest"
   end
 
 end
