a2b2dd9246c48dd958daee96cb7c5be02a769190
Nick Peters <nick@barbetsapp.com>
2015-02-12 16:57:55 -0700

    create registration form and user show page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,7 +8,7 @@ class RegistrationsController < ApplicationController
     @user = User.new(params.require(:user).permit(:email, :password, :password_confirmation))
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to user_path(@user)
     else
       render :new
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,7 +51,7 @@ class UsersController < ApplicationController
 private
 
   def user_params
-    params.require(:user).permit(:name, :hometown, :birthday)
+    params.require(:user).permit(:name, :hometown, :birthday, :password, :email)
   end
 
   def set_task
